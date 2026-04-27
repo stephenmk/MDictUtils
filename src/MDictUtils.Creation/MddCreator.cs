1409e92dc64e8c94e7de028ee5ed6f197eb6ddd9
Stephen Kraus <8003332+stephenmk@users.noreply.github.com>
2026-04-26 23:38:33 -0500

    Add new method to `MddCreator` for adding media files by path (#43)
    Library users can add media resources to new MDD files without first
    loading the data in memory as bytes. The path to the media file on
    disk can be used directly; there's no need to write the data to an
    intermediate file first.
    
    Co-authored-by: stephenmk <stephenmk@users.noreply.github.com>

--- src/MDictUtils.Creation/MddCreator.cs
+++ src/MDictUtils.Creation/MddCreator.cs
@@ -4,6 +4,26 @@ namespace MDictUtils.Creation;
 
 public sealed class MddCreator(string? filepath = null) : MDictCreator(filepath)
 {
+    /// <summary>
+    /// Add a media record to the MDD file.
+    /// </summary>
+    /// <param name="key">Dictionary media URI, e.g. `\images\1.png` or `\audio\sample.mp3`.</param>
+    /// <param name="path">Path to the media data on disk, e.g. `/home/user/media/sample.mp3`.</param>
+    public void AddEntry(string key, string path)
+    {
+        ObjectDisposedException.ThrowIf(_isDisposed, this);
+
+        var info = new FileInfo(path);
+        var size = Convert.ToInt32(info.Length); // Files >2GB are not supported.
+
+        _entries.Add(new(key, path, 0, size));
+    }
+
+    /// <summary>
+    /// Add a media record to the MDD file.
+    /// </summary>
+    /// <param name="key">Dictionary media URI, e.g. `\images\1.png` or `\audio\sample.mp3`.</param>
+    /// <param name="bytes">The media data in bytes.</param>
     public async Task AddEntryAsync(string key, ReadOnlyMemory<byte> bytes)
     {
         ObjectDisposedException.ThrowIf(_isDisposed, this);
@@ -14,6 +34,9 @@ public sealed class MddCreator(string? filepath = null) : MDictCreator(filepath)
         await _stream.WriteAsync(bytes);
     }
 
+    /// <summary>
+    /// Write all added entries to a new MDD file.
+    /// </summary>
     public async Task WriteAsync(MddHeader header, string outputFile, Action<MddWriterOptions>? configure = null)
     {
         ObjectDisposedException.ThrowIf(_isDisposed, this);
